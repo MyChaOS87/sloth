efb0feef4a6718ccfb3542b95ea377a3fc7db337
Michael Kasch <kasch@smartcast.de>
2023-05-22 11:46:13 +0200

    Change URLs for influx, enable tls

--- create-and-seal-influx-secrets.sh
+++ create-and-seal-influx-secrets.sh
@@ -3,7 +3,7 @@ set -e +x
 
 
 
-kubeseal > deployments/influx-secrets/grafana-secrets.sealed.json << EOF
+kubeseal > deployments/influx-secrets/grafana-secret.sealed.json << EOF
 apiVersion: v1
 kind: Secret
 metadata:
@@ -12,6 +12,17 @@ metadata:
 data:
   admin-user: "$(echo -n root | base64)"
   admin-password: "$(pwgen -sc 24  1 | head -c -1 | base64)"
+EOF
+
+kubeseal > deployments/influx-secrets/influx-secret.sealed.json << EOF
+apiVersion: v1
+kind: Secret
+metadata:
+  name: influx-secret
+  namespace: influxdb
+data:
+  admin-password: "$(pwgen -sc 24  1 | head -c -1 | base64)"
+  admin-token: "$(pwgen -sc 24  1 | head -c -1 | base64)"
 EOF
 
 echo Secrets are sealed 1>&2
