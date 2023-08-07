028ce629b0d4b8fdff6697920e12880cf4fba840
Michael Kasch <kasch@smartcast.de>
2023-08-07 16:23:18 +0200

    Add CertManager and ExternalDNS

--- create-and-seal-gcloud-service-accounts.sh
+++ create-and-seal-gcloud-service-accounts.sh
@@ -7,6 +7,15 @@ set -e +x
 kubectl create secret generic gcloud-dns01-solver -n cert-manager \
    --dry-run=client --from-file=.secrets/gcloud-dns01-solver.key.json \
    -o json > .secrets/gcloud-dns01-solver.json
-kubeseal --controller-namespace sealed-secrets < .secrets/gcloud-dns01-solver.json > deployments/cert-manager-resources/gcloud-dns01-solver.sealed.json 
+kubeseal --controller-namespace sealed-secrets < .secrets/gcloud-dns01-solver.json > cluster-essentials/cert-manager-config/gcloud-dns01-solver.sealed.json 
+
+
+[ ! -f .secrets/gcloud-external-dns.key.json ] && gcloud iam service-accounts keys create .secrets/gcloud-external-dns.key.json \
+   --iam-account external-dns@vogelherd.iam.gserviceaccount.com
+
+kubectl create secret generic gcloud-external-dns -n external-dns \
+   --dry-run=client --from-file=.secrets/gcloud-external-dns.key.json \
+   -o json > .secrets/gcloud-external-dns.json
+kubeseal --controller-namespace sealed-secrets < .secrets/gcloud-external-dns.json > cluster-essentials/external-dns-config/gcloud-external-dns.sealed.json 
 
 echo Secrets are sealed 1>&2
