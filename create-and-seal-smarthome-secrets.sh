f8c197246c1f65d6a78809cf9d22ca9e8c75ac51
Michael Kasch <kasch@smartcast.de>
2023-08-08 23:20:39 +0200

    Deploy Smarthome

---
+++ create-and-seal-smarthome-secrets.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -e +x
+
+echo -n create grafana secret... 1>&2
+kubeseal --controller-namespace sealed-secrets > deployments/smarthome/grafana-secrets/grafana-secret.sealed.json << EOF
+apiVersion: v1
+kind: Secret
+metadata:
+  name: grafana-secret
+  namespace: grafana
+data:
+  admin-user: "$(echo -n root | base64)"
+  admin-password: "$(pwgen -sc 24  1 | head -c -1 | base64)"
+EOF
+echo done! 1>&2
+
+echo -n create influxdb secret... 1>&2
+kubeseal --controller-namespace sealed-secrets> deployments/smarthome/influxdb-secrets/influxdb-secret.sealed.json << EOF
+apiVersion: v1
+kind: Secret
+metadata:
+  annotations:
+    sealedsecrets.bitnami.com/cluster-wide: "true"
+  name: influxdb-secret
+data:
+  admin-password: "$(pwgen -sc 24  1 | head -c -1 | base64)"
+  admin-token: "$(pwgen -sc 24  1 | head -c -1 | base64)"
+EOF
+echo done! 1>&2
